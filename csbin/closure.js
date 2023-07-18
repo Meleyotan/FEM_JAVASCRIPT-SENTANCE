ff7c9c2cd8719b13da1a94b22f42db93e1caca0f
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2023-07-18 18:31:01 +0100

    csbin file created, file import from csbin to local

--- csbin/closure.js
+++ csbin/closure.js
@@ -43,20 +43,104 @@ printHello(); // => should console.log('hello');
 // Uncomment those lines of code. Try to deduce the output before executing.
 //Now we are going to create a function addByX that returns a function that will add an input by x.
 
+// CHALLENGE 3
+function outer() {
+  let counter = 0; // this variable is outside incrementCounter's scope
+  function incrementCounter () {
+    counter ++;
+    console.log('counter', counter);
+  }
+  return incrementCounter;
+}
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+// /*** Uncomment these to check your work! ***/
+willCounter();
+willCounter();
+willCounter();
+
+jasCounter();
+willCounter();
+
+
+function addByX(x) {
+  return function addInput(y){
+    return console.log(y = y + x)
+  }
+  return addInput
+}
+
+// /*** Uncomment these to check your work! ***/
+const addByTwo = addByX(2);
+addByTwo(1); // => should return 3
+addByTwo(2); // => should return 4
+addByTwo(3); // => should return 5
+
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
+
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9
+
 // Challenge 4
 // Write a function once that accepts a callback as input and returns a function.
 //When the returned function is called the first time, it should call the callback and
 //return that output.If it is called any additional times, instead of calling the callback
 //again it will simply return the output value from the first time it was called.
 
+// CHALLENGE 4
+function once(func) {
+  let output;
+  function callonce(x){
+    if (output > 0){
+      return (`The output is ${output}`)
+    }
+    else{
+      output = func(x)
+      return (`The output is ${output}`)
+    }
+  }
+  return callonce
+}
+
+// /*** Uncomment these to check your work! ***/
+// const onceFunc = once(addByTwo);
+// console.log(onceFunc(4));  // => should log 6
+// console.log(onceFunc(10));  // => should log 6
+// console.log(onceFunc(9001));  // => should log 6
+
+
 // Challenge 5
 // Write a function after that takes the number of times the callback needs to be called before
 //being executed as the first parameter and the callback as the second parameter.
+
+function after(count, func) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const called = function() { console.log('hello') };
+// const afterCalled = after(3, called);
+// afterCalled(); // => nothing is printed
+// afterCalled(); // => nothing is printed
+// afterCalled(); // => 'hello' is printed
 
 // Challenge 6
 // Write a function delay that accepts a callback as the first parameter and the wait in milliseconds
 //before allowing the callback to be invoked as the second parameter.Any additional arguments
 //after wait are provided to func when it is invoked.HINT: research setTimeout();
+
+function delay(func, wait) {
+  
+}
+
 
 // Challenge 7
 // Write a function rollCall that accepts an array of names and returns a function.
@@ -93,6 +177,17 @@ rollCaller() // => should log 'Everyone accounted for'
 //in as an argument.When this happens, the returned function will return an object with all
 //previously passed -in arguments as keys, and the corresponding outputs as values.
 
+function saveOutput(func, magicWord) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const multiplyBy2 = function(num) { return num * 2; };
+// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+// console.log(multBy2AndLog(2)); // => should log 4
+// console.log(multBy2AndLog(9)); // => should log 18
+// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+
 // Challenge 9
 // Create a function cycleIterator that accepts an array, and returns a function.
 //The returned function will accept zero arguments.When first invoked, the returned
@@ -101,12 +196,33 @@ rollCaller() // => should log 'Everyone accounted for'
 //After returning the last element of the array, the next invocation will return the
 //first element of the array again, and continue on with the second after that, and so forth.
 
+function cycleIterator(array) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+// const getDay = cycleIterator(threeDayWeekend);
+// console.log(getDay()); // => should log 'Fri'
+// console.log(getDay()); // => should log 'Sat'
+// console.log(getDay()); // => should log 'Sun'
+// console.log(getDay()); // => should log 'Fri'
+
 // Challenge 10
 // Create a function defineFirstArg that accepts a function and an argument.
 //Also, the function being passed in will accept at least one argument.defineFirstArg will
 //return a new function that invokes the passed -in function with the passed -in argument as
 //the passed -in function's first argument. Additional arguments needed by the passed-in function
 //will need to be passed into the returned function.
+
+function defineFirstArg(func, arg) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const subtract = function(big, small) { return big - small; };
+// const subFrom20 = defineFirstArg(subtract, 20);
+// console.log(subFrom20(5)); // => should log 15
 
 // Challenge 11
 // Create a function dateStamp that accepts a function and returns a function.
@@ -115,20 +231,66 @@ rollCaller() // => should log 'Everyone accounted for'
 //and an output key that contains the result from invoking the passed -in function.HINT:
 //You may need to research how to access information on Date objects.
 
+function dateStamp(func) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const stampedMultBy2 = dateStamp(n => n * 2);
+// console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
+// console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+
 // Challenge 12
 // Create a function censor that accepts no arguments. censor will return a function that
 //will accept either two strings, or one string.When two strings are given, the returned
 //function will hold onto the two strings as a pair, for future use.When one string is given,
 //the returned function will return the same string, except all instances of first strings(of saved pairs)
 //will be replaced with their corresponding second strings(of those saved pairs).
+
+function censor() {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const changeScene = censor();
+// changeScene('dogs', 'cats');
+// changeScene('quick', 'slow');
+// console.log(changeScene('The quick, brown fox jumps over the lazy dogs.'));
+// => should log 'The slow, brown fox jumps over the lazy cats.'
+
 // Challenge 13
 // There's no such thing as private properties on a JavaScript object! But, maybe there are?
 //Implement a function createSecretHolder(secret) which accepts any value as secret and returns
 //an object with ONLY two methods.getSecret() which returns the secret setSecret() which sets the secret
 
+// CHALLENGE 13
+function createSecretHolder(secret) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// obj = createSecretHolder(5)
+// obj.getSecret() // => returns 5
+// obj.setSecret(2)
+// obj.getSecret() // => returns 2
+
 // Challenge 14
 // Write a function, callTimes, that returns a new function. The new function should return
 //the number of times it’s been called.
+
+// CHALLENGE 14
+function callTimes() {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// let myNewFunc1 = callTimes();
+// let myNewFunc2 = callTimes();
+// myNewFunc1(); // => 1
+// myNewFunc1(); // => 2
+// myNewFunc2(); // => 1
+// myNewFunc2(); // => 2
+
 
 // Challenge 15
 // Create a function roulette that accepts a number (let us call it n), and returns a function.
@@ -137,21 +299,64 @@ rollCaller() // => should log 'Everyone accounted for'
 //the returned function will return the string 'win'.On every invocation after that,
 //the returned function returns the string 'pick a number to play again'.
 
+// CHALLENGE 15
+function roulette(num) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const play = roulette(3);
+// console.log(play()); // => should log 'spin'
+// console.log(play()); // => should log 'spin'
+// console.log(play()); // => should log 'win'
+// console.log(play()); // => should log 'pick a number to play again'
+// console.log(play()); // => should log 'pick a number to play again'
+
 // Challenge 16
-// Create a function average that accepts no arguments, and returns a function 
+// Create a function average that accepts no arguments, and returns a function
 //(that will accept either a number as its lone argument, or no arguments at all).
 //When the returned function is invoked with a number, the output should be average
-//of all the numbers have ever been passed into that function 
+//of all the numbers have ever been passed into that function
 //(duplicate numbers count just like any other number). When the returned function is invoked
-//with no arguments, the current average is outputted.If the returned function is invoked with 
+//with no arguments, the current average is outputted.If the returned function is invoked with
 //no arguments before any numbers are passed in, then it should return 0.
 
+// CHALLENGE 16
+function average() {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const avgSoFar = average();
+// console.log(avgSoFar()); // => should log 0
+// console.log(avgSoFar(4)); // => should log 4
+// console.log(avgSoFar(8)); // => should log 6
+// console.log(avgSoFar()); // => should log 6
+// console.log(avgSoFar(12)); // => should log 8
+// console.log(avgSoFar()); // => should log 8
+
 // Challenge 17
-// Create a function makeFuncTester that accepts an array (of two-element sub-arrays), 
+// Create a function makeFuncTester that accepts an array (of two-element sub-arrays),
 //and returns a function (that will accept a callback). The returned function should return
 //true if the first elements(of each sub - array) being passed into the callback all yield the
-//corresponding second elements(of the same sub - array).Otherwise, the returned function should 
+//corresponding second elements(of the same sub - array).Otherwise, the returned function should
 //return false.
+
+// CHALLENGE 17
+function makeFuncTester(arrOfTests) {
+  
+}
+
+// /*** Uncomment these to check your work! ***/
+// const capLastTestCases = [];
+// capLastTestCases.push(['hello', 'hellO']);
+// capLastTestCases.push(['goodbye', 'goodbyE']);
+// capLastTestCases.push(['howdy', 'howdY']);
+// const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+// const capLastAttempt1 = str => str.toUpperCase();
+// const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
+// console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
+// console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
 
 // Challenge 18
 // Create a function makeHistory that accepts a number (which will serve as a limit), and returns a
@@ -163,15 +368,31 @@ rollCaller() // => should log 'Everyone accounted for'
 //'undone' after(separated by a space).If 'undo' is passed into the function and the function's history is
 //empty, then the function should return the string 'nothing to undo'.
 
+// CHALLENGE 18
+function makeHistory(limit) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const myActions = makeHistory(2);
+// console.log(myActions('jump')); // => should log 'jump done'
+// console.log(myActions('undo')); // => should log 'jump undone'
+// console.log(myActions('walk')); // => should log 'walk done'
+// console.log(myActions('code')); // => should log 'code done'
+// console.log(myActions('pose')); // => should log 'pose done'
+// console.log(myActions('undo')); // => should log 'pose undone'
+// console.log(myActions('undo')); // => should log 'code undone'
+// console.log(myActions('undo')); // => should log 'nothing to undo'
+
 // Challenge 19
 // Inspect the commented out test cases carefully if you need help to understand these instructions.
 
 // Create a function blackjack that accepts an array
-//(which will contain numbers ranging from 1 through 11), and returns a DEALER function. 
+//(which will contain numbers ranging from 1 through 11), and returns a DEALER function.
 //The DEALER function will take two arguments(both numbers), and then return yet ANOTHER
 //function, which we will call the PLAYER function.
 
-// On the FIRST invocation of the PLAYER function, it will return the sum of the two numbers 
+// On the FIRST invocation of the PLAYER function, it will return the sum of the two numbers
 //passed into the DEALER function.
 
 // On the SECOND invocation of the PLAYER function, it will return either:
@@ -180,18 +401,57 @@ rollCaller() // => should log 'Everyone accounted for'
 //passed in as arguments into the DEALER function, IF that sum is 21 or below, OR
 // the string 'bust' if that sum is over 21.
 
-// If it is 'bust', then every invocation of the PLAYER function AFTER THAT will return the string 
+// If it is 'bust', then every invocation of the PLAYER function AFTER THAT will return the string
 //'you are done!'(but unlike 'bust', the 'you are done!' output will NOT use a number in the array).
 //If it is NOT 'bust', then the next invocation of the PLAYER function will return either:
 
 // the most recent sum plus the next number in the array (a new sum) if that new sum is 21 or less, OR
 // the string 'bust' if the new sum is over 21.
 
-// And again, if it is 'bust', then every subsequent invocation of the PLAYER function will return 
+// And again, if it is 'bust', then every subsequent invocation of the PLAYER function will return
 //the string 'you are done!'.Otherwise, it can continue on to give the next sum with the next number
 //in the array, and so forth.
 // You may assume that the given array is long enough to give a 'bust' before running out of numbers.
 
-// BONUS: Implement blackjack so the DEALER function can return more PLAYER functions that will each 
-//continue to take the next number in the array after the previous PLAYER function left off.You will 
+// BONUS: Implement blackjack so the DEALER function can return more PLAYER functions that will each
+//continue to take the next number in the array after the previous PLAYER function left off.You will
 //just need to make sure the array has enough numbers for all the PLAYER functions.
+
+// CHALLENGE 19
+function blackjack(array) {
+
+}
+
+// /*** Uncomment these to check your work! ***/
+
+// /*** DEALER ***/
+// const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+
+// /*** PLAYER 1 ***/
+// const i_like_to_live_dangerously = deal(4, 5);
+// console.log(i_like_to_live_dangerously()); // => should log 9
+// console.log(i_like_to_live_dangerously()); // => should log 11
+// console.log(i_like_to_live_dangerously()); // => should log 17
+// console.log(i_like_to_live_dangerously()); // => should log 18
+// console.log(i_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+
+// /*** BELOW LINES ARE FOR THE BONUS ***/
+
+// /*** PLAYER 2 ***/
+// const i_TOO_like_to_live_dangerously = deal(2, 2);
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+// const i_ALSO_like_to_live_dangerously = deal(3, 7);
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
